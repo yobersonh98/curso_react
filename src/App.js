5721f3af74680ee1409fcfad5d6d5f6680166052
Yoberson Hernandez <yobersonh98@gmail.com>
2020-04-18 17:22:12 -0400

    commit clase 3 video 7 recorriendo arrays

--- src/App.js
+++ src/App.js
@@ -3,6 +3,33 @@ import React from 'react';
 import "./styles/styles.scss"
 import Curso from "./Curso"
 
+const cursos = [
+  {
+    "title" : "React desde cero",
+    "img" : "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
+    "price" : 30,
+    "profesor" : "Beto Quiroga"
+  }, 
+  {
+    "title" : "Drupal desde cero",
+    "img" : "https://edteam-media.s3.amazonaws.com/courses/original/3c84b04b-4f00-4916-b161-ffe5c6132d19.jpg",
+    "price" : 40,
+    "profesor" : "Beto Quiroga"
+  }, 
+  {
+    "title" : "Go desde cero",
+    "img" : "https://edteam-media.s3.amazonaws.com/courses/original/91e149d0-961a-4594-a8ff-0a625be9cdd2.png",
+    "price" : 50,
+    "profesor" : "Alexis Lozada"
+  }, 
+  {
+    "title" : "HTML desde cero",
+    "img" : "https://edteam-media.s3.amazonaws.com/courses/original/f1fa315c-d94a-4fba-b408-cc852a2f4e41.jpg",
+    "price" : 10,
+    "profesor" : "Alvaro Felipe"
+  }
+]
+
 const App = () => (
 <>
   <div className="main-banner img-container l-section" id="main-banner">
@@ -19,8 +46,9 @@ const App = () => (
   </div>
 
   <div className="ed-grid m-grid-3">
-    <Curso title="React desde cero" price= {20}
-    />
+    {
+      cursos.map( c => <Curso title={c.title} image={c.img} price={c.price} profesor={c.profesor} />)
+    }
   </div>
 </>
 )
