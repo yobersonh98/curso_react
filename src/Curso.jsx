45395aa5939f1b88a0b51a63a3d5d2ff2b1040a5
Yoberson Hernandez <yobersonh98@gmail.com>
2020-04-18 14:04:02 -0400

    commit clase 3 video 4 cont propiedades

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 
 
-const curso = {
-    "title" : "React desde cero",
-    "image" : "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
-    "price" : "50usd"
-}
-const Curso = () => (
+
+const Curso = ({title,image,price,profesor}) => (
         <article className="card">
             <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-                <img src={curso.image} alt={curso.title}/>
+                <img src={image} alt={title}/>
             </div>
             <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-                <h3 className="center"> {curso.title} </h3>
+                <h3 className="center"> {title} </h3>
                 <div className="s-main-center">
-                    <a className="button--ghost-alert button--tiny" href="https://ed.team"> {` ${curso.price} $`} </a>
+                    {`Prof.: ${profesor}`}
+                </div>
+                <div className="s-main-center">
+                    <a className="button--ghost-alert button--tiny" href="https://ed.team"> {`$ ${price} `} </a>
                 </div>
             </div>
         </article>
