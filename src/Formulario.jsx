7c17b53f2f8bbcb09920d65b047165ad764671a1
Yoberson Hernandez <yobersonh98@gmail.com>
2020-04-18 19:59:57 -0400

    commit clase 4 video 2 "Formulario"

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -11,6 +11,19 @@ class Formulario extends Component{
         return (
             <div className="ed-grid">
                 <h1>Formulario</h1>
+                <form>
+                    <div className="form__item">
+                        <label>Nombre Completo</label>
+                        <input type="text"/>
+                    </div>
+                    <div className="form__item">
+                        <label>Correo Electrónico</label>
+                        <input type="email"/>
+                    </div>
+                    <div className="form__item">
+                        <input type="submit" value="Enviar" className="button full"/>
+                    </div>
+                </form>
             </div>
         )
     }
