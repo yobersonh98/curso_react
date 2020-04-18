dce1659644ba8a3d10c8032dc75811f441e53f6c
Yoberson Hernandez <yobersonh98@gmail.com>
2020-04-18 12:48:31 -0400

    commit clase 3 video 3 "Propiedades"

--- src/Curso.jsx
+++ src/Curso.jsx
@@ -1,39 +1,20 @@
 import React from 'react'
 
-const persona = {"nombre" : "Yoberson", "apellido" : "Hernandez", "edad": 22}
-const mayorEdad = edad => edad >= 18
+
+const curso = {
+    "title" : "React desde cero",
+    "image" : "https://edteam-media.s3.amazonaws.com/courses/original/5eaaec0b-fa78-4f48-94b6-61b757e1f5fa.png",
+    "price" : "50usd"
+}
 const Curso = () => (
         <article className="card">
             <div className="img-container s-ratio-16-9 s-radius-tr s-radius-tl">
-                <img src="https://edteam-media.s3.amazonaws.com/courses/original/565fa9ec-8f84-448a-b0d8-855d059f16dd.png" alt="Poster del curso"/>
+                <img src={curso.image} alt={curso.title}/>
             </div>
-                <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
-                
-                    {
-                        mayorEdad(persona.edad) 
-                        ? 
-                            <div>
-                                <h1>
-                                    <span>
-                                        Hola, soy mayor
-                                    </span>
-                                </h1>
-                            </div> 
-                        : <p>soy menor</p>
-                    }
-                
-                <div className="s-mb-2 s-main-center">
-                    <div className="card__teacher s-cross-center">
-                    <div className="card__avatar s-mr-1">
-                        <div className="circle img-container">
-                        <img src="https://edteam-media.s3.amazonaws.com/users/avatar/e19b1e7f-6dcc-495b-883c-7d0b9b3fdf15.jpg" alt="Tio Beto"/>
-                        </div>
-                    </div>
-                    <span className="small"> {`${persona.nombre} ${persona.apellido}`} </span>
-                    </div>
-                </div>
+            <div className="card__data s-border s-radius-br s-radius-bl s-pxy-2">
+                <h3 className="center"> {curso.title} </h3>
                 <div className="s-main-center">
-                    <a className="button--ghost-alert button--tiny" href="https://ed.team">$ 20USD</a>
+                    <a className="button--ghost-alert button--tiny" href="https://ed.team"> {` ${curso.price} $`} </a>
                 </div>
             </div>
         </article>
