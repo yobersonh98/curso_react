08fb0731f2757a872e6976f6b9d2be096a843a7a
Yoberson Hernandez <yobersonh98@gmail.com>
2020-04-19 10:04:45 -0400

    Commit clase 4 video final "ciclo de vida de los componentes"

--- src/Formulario.jsx
+++ src/Formulario.jsx
@@ -7,10 +7,12 @@ class Formulario extends Component{
 
         this.state = {
             nombre : "",
-            correo : ""
+            correo : "",
+            fecha : new Date()
         }
         this.cambiarNombre = this.cambiarNombre.bind(this)
         this.cambiarCorreo = this.cambiarCorreo.bind(this)
+        this.cambiarFecha = this.cambiarFecha.bind(this)
     }
 
     cambiarNombre(e){
@@ -25,6 +27,12 @@ class Formulario extends Component{
         })
     }
 
+    cambiarFecha(){
+        this.setState({
+            fecha : new Date()
+        })
+    }
+
     //this.setState({})  este metodo recibe como parametro "{objeto}" el cual se actualiza a this.state
 
     //Metodo render returna el contenido como tal
@@ -32,7 +40,8 @@ class Formulario extends Component{
         return (
             <div className="ed-grid">
                 <h1>Formulario</h1>
-                <form>
+                <h3>Fecha Actual: {Math.ceil(this.state.fecha)}</h3>
+                <form id="elemento">
                     <div className="ed-grid m-grid-2">
                         <div className="form__item">
                             <label>Nombre Completo</label>
@@ -56,6 +65,24 @@ class Formulario extends Component{
         )
     }
 
+    componentDidMount(){
+        let elemento = document.getElementById("elemento")
+        
+        this.intervaloFecha = setInterval(() => {
+            this.cambiarFecha()
+            console.log(new Date())
+        }, 1000)
+    }
+
+    componentDidUpdate(prevProps, prevState){
+        //console.log(prevState)
+    }
+
+    componentWillUnmount(){
+        clearInterval(this.intervaloFecha)
+    }
 }
+
+
 
 export default Formulario
